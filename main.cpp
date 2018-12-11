433c0c70a0cc10ad6d3d5f21ae3801e20241d791
Jerrypoi <jerry.poi.sz@gmail.com>
2018-12-11 22:57:54 +0800

    add command line support

--- main.cpp
+++ main.cpp
@@ -3,9 +3,21 @@
 #include <string>
 #include "Compress.h"
 using namespace std;
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    compress("pikachu_testpass.txt","out.cps");
-    decompress("out.cps","recover.txt");
+int main(int argc,char *args[]) {
+    cout<<"argc = "<<argc<<endl;
+    cout<<"args[0] = " <<args[0]<<endl;
+    if(argc == 1) {
+        compress("pikachu_testpass.txt","out.cps");
+        decompress("out.cps","recover.txt");
+        return 0;
+    }
+    else if(argc != 4) {
+        return -1;
+    }
+    if(string(args[1]) == "-c") {
+        compress(args[2],args[3]);
+    } else if(string(args[1]) == "-d") {
+        decompress(args[2],args[3]);
+    }
     return 0;
 }
